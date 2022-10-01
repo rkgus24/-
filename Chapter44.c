fc6299b7bcd382af6bcfef313f340f1067cdec5a
가현 <102145605+rkgus24@users.noreply.github.com>
2022-10-01 15:17:05 +0900

    Update Chapter44.c

---
+++ Chapter44.c
@@ -0,0 +1,17 @@
+//하향식
+
+#include <stdio.h>
+#include <stdlib.h>
+
+long long int n;
+
+int f(long long int n) {
+    if (n == 0) return 0;
+    return (n%10) + f(n/10);
+}
+
+int main() {
+    scanf("%lld", &n);
+    printf("%d\n", f(n));
+    return 0;
+}
