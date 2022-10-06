991329c3695f8957033d3229b205e7717a9fc3f5
가현 <102145605+rkgus24@users.noreply.github.com>
2022-10-06 15:31:05 +0900

    Update queue2.c

--- queue2.c
+++ queue2.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 typedef int element ;
 typedef struct {
 	element* data;
@@ -7,6 +8,7 @@ typedef struct {
 	int rear;
 	int size;
 }Queue;
+
 Queue *initQueue( int n );
 int isFull( Queue *q );
 void enQueue( Queue *q, element data );
@@ -14,19 +16,18 @@ int isEmpty( Queue *q );
 element deQueue( Queue *q );
 void destroyQueue( Queue * q );
 void printAll( Queue *q );
+
 int main() {
 	Queue *q;
 	element data;
 	q = initQueue( 5 );
-	enQueue( q, 10 ) ;
-	enQueue( q, 20 ) ;
-	enQueue( q, 30 ) ;
-	enQueue( q, 40 ) ;
-	enQueue( q, 50 ) ;
-	data = deQueue( q );
-	if( data ) printf("%d\n", data );
-	enQueue( q, 60 ) ;
-	printAll( q );
+	enQueue( q, 10 ) ; printAll(q);
+	enQueue( q, 20 ) ; printAll(q);
+	enQueue( q, 30 ) ; printAll(q);
+	enQueue( q, 40 ) ; printAll(q);
+	enQueue( q, 50 ) ; printAll(q);
+	data = deQueue( q ) ; printAll(q);
+	enQueue( q, 60 ) ; printAll(q);
 	destroyQueue( q );
 	return 0;
 }
@@ -35,14 +36,14 @@ Queue* initQueue( int n ){
 	temp -> size = n; // n을 크기로 하는 큐
 	temp -> rear = -1; 
 	temp -> front = -1;
-	temp -> data = (element *)malloc(sizeof(element)* temp->size); // 크기 n 을 가지는 동적 배열 생성
+	temp -> data = (element *)malloc(sizeof(element)* n); // 크기 n 을 가지는 동적 배열 생성 n --> temp->size;
 	return temp; 
 }
 int isFull( Queue *q ){
 	return q->rear + 1 == q->size; // 큐가 가득 차 있다면 rear 은 어떤 상태일까?
 }
 void enQueue( Queue *q, element data ){
-	 if (isFull(q)) printf("Queue is full"); // 가득 차 있다면 "queue is full"이라고 출력
+	 if (isFull(q)) printf("Queue is full\n"); // 가득 차 있다면 "queue is full"이라고 출력
      else {  // 아니면 rear에 data 삽입
         q->rear++;
         q->data[q->rear] = data;
@@ -64,7 +65,7 @@ element deQueue( Queue *q ){ // 비어 있다면 "Queue is empty" 출력하고 0
 void printAll( Queue *q ){
 	// front 부터 rear 까지 모든 데이터 출력
     int i;
-	for(i=0;i<q->size i++){
+	for(i=0;i<q->size; i++){
 		if (i<=q->front || i > q->rear){
 			printf(" | ");
 		}
